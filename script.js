3504ee2c1d959390848be33e344d1fb3f0d1a887
lawrencenjeri4-lgtm <lawrencenjeri4@gmail.com>
2026-06-20 21:38:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,19 @@ const lightsBtn = document.getElementById("lightsBtn");
 
 const welcome = document.getElementById("welcome");
 const lightsScreen = document.getElementById("lightsScreen");
+const balloons = document.getElementById("balloons");
 
 startBtn.addEventListener("click", () => {
+
     welcome.classList.add("hidden");
     lightsScreen.classList.remove("hidden");
+
 });
 
 lightsBtn.addEventListener("click", () => {
+
     document.body.classList.add("lights-on");
+
+    balloons.classList.remove("hidden");
+
 });
