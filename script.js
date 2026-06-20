c55feb9927baf5138e5b71a827883cce31bc57e5
lawrencenjeri4-lgtm <lawrencenjeri4@gmail.com>
2026-06-20 21:16:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const startBtn = document.getElementById("startBtn");
+const lightsBtn = document.getElementById("lightsBtn");
+
+const welcome = document.getElementById("welcome");
+const lightsScreen = document.getElementById("lightsScreen");
 
 startBtn.addEventListener("click", () => {
-    alert("Welcome Katchee ❤️");
+    welcome.classList.add("hidden");
+    lightsScreen.classList.remove("hidden");
+});
+
+lightsBtn.addEventListener("click", () => {
+    document.body.classList.add("lights-on");
 });
